aae95b9adfde2ee309ba0e297232c08383fa849b
fgolf <fontas.gkolf@gmail.com>
2020-02-05 21:16:55 +0200

    BottomMenu cleanup

--- src/views/Menu/BottomMenu.js
+++ src/views/Menu/BottomMenu.js
@@ -1,17 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FloatingAction } from 'react-native-floating-action';
-import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
-
-const styles = StyleSheet.create({
-  toggler: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: 30,
-    right: 0,
-    backgroundColor: '#F3D994'
-  }
-});
 
 const actions = [
   {
@@ -26,31 +14,19 @@ const actions = [
   }
 ];
 
-const BottomMenu = ({ addPlayer, removePlayer }) => {
-  const [visible, setVisible] = useState(true);
-  return (
-    <>
-      <TouchableOpacity
-        style={styles.toggler}
-        onPress={() => setVisible(!visible)}
-      >
-        <Text>toggle</Text>
-      </TouchableOpacity>
-      <FloatingAction
-        color="#F3D994"
-        actions={actions}
-        visible={visible}
-        onPressItem={name => {
-          if (name === 'add_player') {
-            addPlayer();
-          }
-          if (name === 'remove_player') {
-            removePlayer();
-          }
-        }}
-      />
-    </>
-  );
-};
+const BottomMenu = ({ addPlayer, removePlayer }) => (
+  <FloatingAction
+    color="#F3D994"
+    actions={actions}
+    onPressItem={name => {
+      if (name === 'add_player') {
+        addPlayer();
+      }
+      if (name === 'remove_player') {
+        removePlayer();
+      }
+    }}
+  />
+);
 
 export default BottomMenu;
