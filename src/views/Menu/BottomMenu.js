23d53efee1c961f446e3e424712b79bf004fd349
fgolf <fontas.gkolf@gmail.com>
2020-03-25 17:30:19 +0200

    added reset functionality on menu

--- src/views/Menu/BottomMenu.js
+++ src/views/Menu/BottomMenu.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FloatingAction } from 'react-native-floating-action';
+import { store, endGameAction } from '../../store/store';
 
 const actions = [
+  {
+    text: 'Reset Game',
+    name: 'reset_game',
+    color: '#F3D994'
+  },
   {
     text: 'Add Player',
     name: 'add_player',
@@ -14,19 +20,26 @@ const actions = [
   }
 ];
 
-const BottomMenu = ({ addPlayer, removePlayer }) => (
-  <FloatingAction
-    color="#F3D994"
-    actions={actions}
-    onPressItem={name => {
-      if (name === 'add_player') {
-        addPlayer();
-      }
-      if (name === 'remove_player') {
-        removePlayer();
-      }
-    }}
-  />
-);
+const BottomMenu = ({ addPlayer, removePlayer }) => {
+  const { dispatch } = useContext(store);
+
+  return (
+    <FloatingAction
+      color="#F3D994"
+      actions={actions}
+      onPressItem={name => {
+        if (name === 'add_player') {
+          addPlayer();
+        }
+        if (name === 'remove_player') {
+          removePlayer();
+        }
+        if (name === 'reset_game') {
+          dispatch(endGameAction());
+        }
+      }}
+    />
+  );
+};
 
 export default BottomMenu;
